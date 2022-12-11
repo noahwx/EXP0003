4ab9f74154545baac4d8a3263d481ff840d892de
Noah Fontenette <noahfontenette@outlook.com>
2022-12-10 19:27:30 -0600

    0.0.015

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,7 +2,7 @@ import './index.css';
 import Main from './Main';
 import Navbar from './Navbar';
 
-// Build 0.0.012
+// Build 0.0.015
 
 function App() {
   return (
--- my-app/src/pages/ContactUs.js
+++ my-app/src/pages/ContactUs.js
@@ -1,9 +1,25 @@
 import '../index.css'
+import '../styles/ContactUs.css'
 
 const ContactUs = () => {
     return (
-        <div>
-            <h1>Contact Us</h1>
+        <div className='container'>
+            <h1 className='CU-title'>Contact Us</h1>
+            <form className='CU-form'>
+                <label className='CU-label'>
+                    Name
+                    <input type='text' name="name" className='CU-input'/>
+                </label>
+                <label className='CU-label'>
+                    Email
+                    <input type='text' name="email" className='CU-input'/>
+                </label>
+                <label className='CU-label'>
+                    Message
+                    <textarea type='text' name="message" className='CU-textarea'/>
+                </label>
+                <input type="submit" value="Submit" className='CU-submit'/>
+            </form>
         </div>
     );
 }
